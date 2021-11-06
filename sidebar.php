096c2155dabff460d48383ca3e67aa256f4beb2a
Daniel Gomez Checa <mardukecheka@gmail.com>
2021-11-06 22:09:08 +0100

    0611

--- sidebar.php
+++ sidebar.php
@@ -12,12 +12,12 @@
 					</button>
 					<div class="collapse" id="tema3">
 					<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-					<li><a href="/tema3/Practica1Tema3/Ejercicio1/index.php" class="link-dark rounded">Ejercicio1</a></li>
-					<li><a href="/tema3/Practica1Tema3/Ejercicio2/" class="link-dark rounded">Ejercicio2</a></li>
-					<li><a href="/tema3/Practica1Tema3/Ejercicio3/" class="link-dark rounded">Ejercicio3</a></li>
-					<li><a href="/tema3/Practica1Tema3/Ejercicio4/" class="link-dark rounded">Ejercicio4</a></li>
-					<li><a href="/tema3/Practica2Tema3/ahorcado/" class="link-dark rounded">Ahorcado</a></li>
-					<li><a href="/tema3/Practica2Tema3/bingo/" class="link-dark rounded">Bingo</a></li>								
+					<li><a href="tema3/Practica1Tema3/Ejercicio1/index.php" class="link-dark rounded">Ejercicio1</a></li>
+					<li><a href="tema3/Practica1Tema3/Ejercicio2/index.php" class="link-dark rounded">Ejercicio2</a></li>
+					<li><a href="tema3/Practica1Tema3/Ejercicio3/index.php" class="link-dark rounded">Ejercicio3</a></li>
+					<li><a href="tema3/Practica1Tema3/Ejercicio4/index.php" class="link-dark rounded">Ejercicio4</a></li>
+					<li><a href="tema3/Practica2Tema3/ahorcado/index.php" class="link-dark rounded">Ahorcado</a></li>
+					<li><a href="tema3/Practica2Tema3/bingo/index.php" class="link-dark rounded">Bingo</a></li>								
 					</ul>
 					</div>
 				</li>
