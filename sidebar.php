bf75a304724991b9cc5a3d25e730191250880599
Daniel Gomez Checa <mardukecheka@gmail.com>
2021-11-06 21:57:23 +0100

    0611

---
+++ sidebar.php
@@ -0,0 +1,25 @@
+
+      		<div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+				<a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+				<svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+				<span class="fs-5 fw-semibold text-primary">EJERCICIOS PHP</span>
+				</a>
+
+				<ul class="list-unstyled ps-0">
+				<li class="mb-1">
+					<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#tema3" aria-expanded="false">
+					TEMA 3
+					</button>
+					<div class="collapse" id="tema3">
+					<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+					<li><a href="/tema3/Practica1Tema3/Ejercicio1/" class="link-dark rounded">Ejercicio1</a></li>
+					<li><a href="/tema3/Practica1Tema3/Ejercicio2/" class="link-dark rounded">Ejercicio2</a></li>
+					<li><a href="/tema3/Practica1Tema3/Ejercicio3/" class="link-dark rounded">Ejercicio3</a></li>
+					<li><a href="/tema3/Practica1Tema3/Ejercicio4/" class="link-dark rounded">Ejercicio4</a></li>
+					<li><a href="/tema3/Practica2Tema3/ahorcado/" class="link-dark rounded">Ahorcado</a></li>
+					<li><a href="/tema3/Practica2Tema3/bingo/" class="link-dark rounded">Bingo</a></li>								
+					</ul>
+					</div>
+				</li>
+				</ul>
+			</div>
