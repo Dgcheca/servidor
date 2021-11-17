216b6ed12e597c26ff6a6c87739a77aedb7c1e4a
Daniel Gomez Checa <mardukecheka@gmail.com>
2021-11-17 20:11:46 +0100

    1711

--- sidebar.php
+++ sidebar.php
@@ -10,12 +10,12 @@
 					</button>
 					<div class="collapse" id="tema3">
 					<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-					<li><a href="/servidor/Tema3/Practica1Tema3/Ejercicio1/index.php" class="link-dark rounded">Ejercicio1</a></li>
-					<li><a href="/servidor/Tema3/Practica1Tema3/Ejercicio2/index.php" class="link-dark rounded">Ejercicio2</a></li>
-					<li><a href="/servidor/Tema3/Practica1Tema3/Ejercicio3/index.php" class="link-dark rounded">Ejercicio3</a></li>
-					<li><a href="/servidor/Tema3/Practica1Tema3/Ejercicio4/index.php" class="link-dark rounded">Ejercicio4</a></li>
-					<li><a href="/servidor/Tema3/Practica2Tema3/ahorcado/" class="link-dark rounded">Ahorcado</a></li>
-					<li><a href="/servidor/Tema3/Practica2Tema3/bingo/index.php" class="link-dark rounded">Bingo</a></li>								
+					<li><a href="Tema3/Practica1Tema3/Ejercicio1/index.php" class="link-dark rounded">Ejercicio1</a></li>
+					<li><a href="Tema3/Practica1Tema3/Ejercicio2/index.php" class="link-dark rounded">Ejercicio2</a></li>
+					<li><a href="Tema3/Practica1Tema3/Ejercicio3/index.php" class="link-dark rounded">Ejercicio3</a></li>
+					<li><a href="Tema3/Practica1Tema3/Ejercicio4/index.php" class="link-dark rounded">Ejercicio4</a></li>
+					<li><a href="Tema3/Practica2Tema3/ahorcado/" class="link-dark rounded">Ahorcado</a></li>
+					<li><a href="Tema3/Practica2Tema3/bingo/index.php" class="link-dark rounded">Bingo</a></li>								
 					</ul>
 					</div>
 				</li>
@@ -25,11 +25,11 @@
 					</button>
 					<div class="collapse" id="tema4">
 					<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-					<li><a href="/servidor/Tema4/Practica1Tema4/Diccionario/index.php" class="link-dark rounded">Diccionario</a></li>
-					<li><a href="/servidor/Tema4/Practica1Tema4/GestionDeTareas/index.php" class="link-dark rounded">GestionDeTareas</a></li>
-					<li><a href="/servidor/Tema4/Practica1Tema4/LyGWebsCompletas/index.php" class="link-dark rounded">LyGWebsCompletas</a></li>
-					<li><a href="/servidor/Tema4/Practica1Tema4/Tienda/index.php" class="link-dark rounded">Tienda</a></li>		
-					<li><a href="/servidor/Tema4/CarritoPDF/index.php" class="link-dark rounded">TiendaV2 (con pdf y email)</a></li>				
+					<li><a href="Tema4/Practica1Tema4/Diccionario/index.php" class="link-dark rounded">Diccionario</a></li>
+					<li><a href="Tema4/Practica1Tema4/GestionDeTareas/index.php" class="link-dark rounded">GestionDeTareas</a></li>
+					<li><a href="Tema4/Practica1Tema4/LyGWebsCompletas/index.php" class="link-dark rounded">LyGWebsCompletas</a></li>
+					<li><a href="Tema4/Practica1Tema4/Tienda/index.php" class="link-dark rounded">Tienda</a></li>		
+					<li><a href="Tema4/CarritoPDF/index.php" class="link-dark rounded">TiendaV2 (con pdf y email)</a></li>				
 					</ul>
 					</div>
 				</li>
